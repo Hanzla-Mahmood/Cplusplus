24a387e50d3a4dc2cc7b494de099ac42ed75b09e
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-08 15:18:13 +0500

    Add files via upload

---
+++ Display first five Numbers and their Squares.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	// Display first five Numbers and their Squares
+	
+	int n=1;
+	
+	while(n<=5){
+		cout<<n<<"    "<<n*n<<endl;
+		n++;
+	}
+}
