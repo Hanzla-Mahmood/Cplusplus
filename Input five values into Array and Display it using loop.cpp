fb862cd417048416609680b6de580bde81de7e63
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-08 14:11:53 +0500

    Add files via upload

---
+++ Input five values into Array and Display it using loop.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+  //Input five values into Array and Display it using loop
+  
+  int arr[5];
+  
+    cout<<"Enter values in array"<<endl;
+    
+   for(int i=0;i<5;i++){
+   	
+  cin>>arr[i];
+ 
+   }
+
+    cout<<"Values into array is"<<endl;
+
+    for(int i=0;i<5;i++){
+    	
+    cout<<arr[i]<<endl;
+ 
+}
+
+}
+    
