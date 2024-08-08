9e3e32144c5d207912dfcb3282b1f6925ea79991
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-08 15:15:18 +0500

    Add files via upload

---
+++ Display 1 to 10 counting with while loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	// Display 1 to 10 counting with while loop
+	
+	int n = 1;
+	
+	while(n<=10){
+		
+		cout<<n<<endl;
+		n++;
+		
+	}
+	
+	
+}
