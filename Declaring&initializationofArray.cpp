213df42dceb61a8e86c3146c9f7d65ecd10bc022
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-08 14:12:04 +0500

    Add files via upload

---
+++ Declaring&initializationofArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+  //  Declaring & Initialization of Array
+  
+//  int arr [5] = {10,20,30,40,50}; one way 
+
+
+// 2nd way 
+ int arr [5];
+  
+  arr[0]= 10;
+  arr[1]=20;
+  arr[2]= 10;
+  arr[3]=20;
+  arr[4]= 10;
+  
+  cout<<arr[1];  
+}
