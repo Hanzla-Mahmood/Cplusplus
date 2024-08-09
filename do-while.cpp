bc28e8baedb2e270a67a8892caf94b4682abc862
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-09 18:35:55 +0500

    Add files via upload

---
+++ do-while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//do-while
+	
+	int n=1;
+	do{
+		cout<<n<<endl;
+		n++;
+	}
+	while(n<=10);
+}
+
+// Do while loop  is executed at once even if it is false , semicolon is used with while ,do while is called posttest condition
