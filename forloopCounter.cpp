06b2b686ba8b0e9fb015157952e6d9b44c81a924
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-09 17:45:42 +0500

    Add files via upload

---
+++ forloopCounter.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int counter;
+	
+	cout<<"how much value too print: "<<endl;
+	cin>>counter;
+	
+	for(int i=1;i<=counter;i++){
+		
+		cout<<i<<endl;
+		
+	}
+}
