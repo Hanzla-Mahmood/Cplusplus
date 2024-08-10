244a4905594238b1279d2f2a85900635c15ec0e2
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-10 16:02:56 +0500

    Add files via upload

---
+++ BreakStatement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+// Break statement is used to exit the loop body.
+
+for(int x=1;x<=5;x++){
+	
+	cout<<"Hello"<<endl;
+	
+	break;
+	
+	cout<<"world"<<endl;
+}
+
+
+}
