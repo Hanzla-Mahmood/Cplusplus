29fbdc8190abcec155eba66c8f98a3562ce2fc53
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-10 16:51:53 +0500

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+void display();  //function define
+
+
+using namespace std;
+int main()
+{
+   
+   display();  // function call
+   
+}
+
+void display(){ // function definition
+	
+	cout<<"My first function";
+}
