901d37e9dde9e12a5cad5788aa75afa567074fed
Hanzla <157579324+Hanzla-Mahmood@users.noreply.github.com>
2024-08-10 16:02:40 +0500

    Add files via upload

---
+++ Continue statement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+// Continue statement is used in the body of the loop.It is used to move the control to the start of the loop body;
+
+for(int x=1;x<=5;x++){
+	
+	cout<<"Hello"<<endl;
+	
+	continue;
+	
+	cout<<"world"<<endl;
+}
+
+
+}
